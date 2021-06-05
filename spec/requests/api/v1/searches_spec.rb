28dfce2c9ff22b0c1f1cdcface7ab2f09f8d01d4
Khoa Nguyen <khoang323@hotmail.com>
2021-06-04 20:37:29 -0600

    fixed spec

--- spec/requests/api/v1/searches_spec.rb
+++ spec/requests/api/v1/searches_spec.rb
@@ -34,6 +34,7 @@ RSpec.describe "Api::V1::Searches", type: :request do
       it "should return data with multiple params" do
         valid_params = {name: 'itc', usd_price: 5, percent_change: 2}
         post '/api/v1/searches', params: valid_params, headers: valid_headers, as: :json
+        expect(response).to be_successful
         body = JSON.parse(response.body, symbolize_names: true)
         expect(body[:data].size).to eq(1)
       end
